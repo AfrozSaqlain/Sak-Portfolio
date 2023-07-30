eadd592ae2172824e1f93f96c562e9dea56037b0
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-07-30 23:22:56 +0530

    Added Head

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 //next image
+import Head from "next/head";
 import Image from "next/image";
 
 //components
@@ -16,65 +17,71 @@ import { fadeIn } from '../variants'
 
 const Home = () => {
   return (
-    <div className='bg-primary/60 h-full'>
-      {/*text*/}
-      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
-        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
-          {/* title */}
-          <motion.h2
-            variants={fadeIn('down', 0.2)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='h2'>
-            Transforming Ideas <br /> Into {' '}
-            <span className='text-accent'>Reality</span>
-          </motion.h2>
-          {/* subtitle */}
-          <motion.p
-            variants={fadeIn('down', 0.3)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
-            In the vastness of space and time, Our love is like a quantum rhyme, Entangled forever, yours and mine.
-            <br /> <br />
-            As we journey through life&rsquo;s great expanse, Our love will grow with each quantum dance, A bond that nothing can ever chance.
-            <br /> <br />
-            From the smallest particles to the stars above, Our love is bound by quantum love, A force that nothing can ever shove.
-          </motion.p>
-          {/* btn */}
-          <div className="flex justify-center xl:hidden relative">
-            <ProjectsBtn />
+    <>
+      <Head>
+        <title>Saqlain Afroz</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className='bg-primary/60 h-full'>
+        {/*text*/}
+        <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+          <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+            {/* title */}
+            <motion.h2
+              variants={fadeIn('down', 0.2)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              className='h2'>
+              Transforming Ideas <br /> Into {' '}
+              <span className='text-accent'>Reality</span>
+            </motion.h2>
+            {/* subtitle */}
+            <motion.p
+              variants={fadeIn('down', 0.3)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+              In the vastness of space and time, Our love is like a quantum rhyme, Entangled forever, yours and mine.
+              <br /> <br />
+              As we journey through life&rsquo;s great expanse, Our love will grow with each quantum dance, A bond that nothing can ever chance.
+              <br /> <br />
+              From the smallest particles to the stars above, Our love is bound by quantum love, A force that nothing can ever shove.
+            </motion.p>
+            {/* btn */}
+            <div className="flex justify-center xl:hidden relative">
+              <ProjectsBtn />
+            </div>
+            <motion.div
+              variants={fadeIn('down', 0.4)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              className="hidden xl:flex"
+            >
+              <ProjectsBtn />
+            </motion.div>
           </div>
+        </div>
+        {/* Image */}
+        <div className="w-[1200px] h-full absolute right-0 bottom-0">
+          {/* bg img */}
+          <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+          {/* particles */}
+          <ParticlesContainer />
           <motion.div
-            variants={fadeIn('down', 0.4)}
+            variants={fadeIn('up', 0.5)}
             initial='hidden'
             animate='show'
             exit='hidden'
-            className="hidden xl:flex"
-          >
-            <ProjectsBtn />
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+            <Avatar />
           </motion.div>
         </div>
       </div>
-      {/* Image */}
-      <div className="w-[1200px] h-full absolute right-0 bottom-0">
-        {/* bg img */}
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
-        {/* particles */}
-        <ParticlesContainer />
-        <motion.div
-          variants={fadeIn('up', 0.5)}
-          initial='hidden'
-          animate='show'
-          exit='hidden'
-          transition={{ duration: 1, ease: 'easeInOut' }}
-          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
-          <Avatar />
-        </motion.div>
-      </div>
-    </div>
+    </>
   );
 };
 
