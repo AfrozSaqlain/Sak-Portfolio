32e4600dcf3fc173e4a0b254337f5e4f96a6bb9c
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-18 23:42:02 +0530

    Added Head in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 //components
 import Layout from '../components/Layout';
 import Transition from '../components/Transition';
 
 // router
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
 //framer motion
 import { AnimatePresence, motion } from 'framer-motion';
@@ -13,14 +14,37 @@ import { AnimatePresence, motion } from 'framer-motion';
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
-  <Layout>
-  <AnimatePresence mode='wait'>
-  <motion.div key = {router.route} className='h-full'>
-  <Transition />
-  <Component {...pageProps} />
-  </motion.div>
-  </AnimatePresence>
-  </Layout>
+    <>
+      <Head>
+        <title>Saqlain Afroz</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Discover a rich blend of financial investment tips, fashion inspiration, web development insights, and comprehensive academic course notes." />
+        <meta name="keywords" content="financial investment, fashion, web development, academic courses, tips, diy, ai/ml, trends, insights, notes, astrophysics, astronomy, maths, mathematics, quantum physics, blogging, blog"></meta>
+        <meta name="author" content="Saqlain Afroz"></meta>
+
+        <meta name="robots" content="index, follow"></meta>
+        <meta name="googlebot" content="index, follow"></meta>
+
+        <meta name="p:domain_verify" content="45d9f00dc549509d8c1964322f2fad87" />
+
+        {/* Open Graph Meta Tags (for sharing on social media) */}
+        <meta property="og:title" content="Saqlain Afroz Portfolio Webapp" />
+        <meta property="og:description" content="Discover a rich blend of financial investment tips, fashion inspiration, web development insights, and comprehensive academic course notes in our Next.js app. Elevate your knowledge across multiple domains with our curated content." />
+        <meta property="og:image" content="profile-pic.png" />
+        <meta property="og:url" content="https://sak-portfolio.vercel.app/" />
+        <meta property="og:type" content="website"></meta>
+
+        <link rel="icon" href="/favicon.png" type="image/x-icon"></link>
+      </Head>
+      <Layout>
+        <AnimatePresence mode='wait'>
+          <motion.div key={router.route} className='h-full'>
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-//next image
-import Head from "next/head";
-import Image from "next/image";
-
 //components
 import ParticlesContainer from '../components/ParticlesContainer';
 import ProjectsBtn from '../components/ProjectsBtn';
@@ -18,27 +14,6 @@ import { fadeIn } from '../variants'
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Saqlain Afroz</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Discover a rich blend of financial investment tips, fashion inspiration, web development insights, and comprehensive academic course notes in our Next.js app. Elevate your knowledge across multiple domains with our curated content." />
-        <meta name="keywords" content="financial investment, fashion, web development, academic courses, tips, diy, ai/ml, trends, insights, notes"></meta>
-        <meta name="author" content="Saqlain Afroz"></meta>
-
-        <meta name="robots" content="index, follow"></meta>
-        <meta name="googlebot" content="index, follow"></meta>
-
-        <meta name="p:domain_verify" content="45d9f00dc549509d8c1964322f2fad87"/>
-
-        {/* Open Graph Meta Tags (for sharing on social media) */}
-      <meta property="og:title" content="Saqlain Afroz Portfolio Webapp"/>
-      <meta property="og:description" content="Discover a rich blend of financial investment tips, fashion inspiration, web development insights, and comprehensive academic course notes in our Next.js app. Elevate your knowledge across multiple domains with our curated content."/>
-      <meta property="og:image" content="profile-pic.png"/>
-      <meta property="og:url" content="https://sak-portfolio.vercel.app/"/>
-      <meta property="og:type" content="website"></meta>
-
-        <link rel="icon" href="favicon.png" type="image/x-icon"></link>
-      </Head>
       <div className='bg-primary/60 h-full'>
         {/*text*/}
         <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
