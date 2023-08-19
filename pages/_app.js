22d02e71e2c6663a556587c98d3645caba1df0aa
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-19 17:01:28 +0530

    Changed og:image path

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }) {
         {/* Open Graph Meta Tags (for sharing on social media) */}
         <meta property="og:title" content="Saqlain Afroz Portfolio Webapp" />
         <meta property="og:description" content="Discover a rich blend of financial investment tips, fashion inspiration, web development insights, and comprehensive academic course notes in our Next.js app. Elevate your knowledge across multiple domains with our curated content." />
-        <meta property="og:image" content="profile-pic.png" />
+        <meta property="og:image" content="../public/profile-pic.png" />
         <meta property="og:url" content="https://sak-portfolio.vercel.app/" />
         <meta property="og:type" content="website"></meta>
 
