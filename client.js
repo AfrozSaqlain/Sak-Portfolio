fc8598d9a82cb67659e120c54838355294c02d6d
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-21 01:56:19 +0530

    Testing sanity

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
-import sanityClient from '@sanity/client'
+import { createClient } from '@sanity/client'
 
-export default sanityClient({
+const client = createClient({
     projectId: 'n2x4qif3',
-    dataset: 'production',// or the name you chose in step 1
-    useCdn: true, // `false` if you want to ensure fresh data
-    API_version: '2021-03-25'
+    dataset: 'production',
+    useCdn: true
 })
+
+export default client
