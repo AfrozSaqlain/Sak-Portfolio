262fa44abda1d7ad1fb0ee839618f3dd9cb44074
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-21 01:43:26 +0530

    Rest

--- pages/blogs/stock_market/index.js
+++ pages/blogs/stock_market/index.js
@@ -1,40 +1,51 @@
-import Link from 'next/link'
-import groq from 'groq'
-import client from '../../../client'
+// import Link from 'next/link'
+// import groq from 'groq'
+// import client from '../../../client'
 
-const Index = ({ posts }) => {
+// const Index = ({ posts }) => {
+//   return (
+//     <div className='relative mx-4 md:mx-24 lg:mx-96 mt-16 md:mt-24 lg:mt-40'>
+//       <h1 className='text-3xl mb-6'>Welcome to a blog!</h1>
+//       <ul>
+//         {posts.length > 0 && posts.map(
+//           ({ _id, title = '', slug = '', publishedAt = '' }) =>
+//             slug && (
+//               <Link key={_id} href={`/blogs/stock_market/post/${encodeURIComponent(slug.current)}`} legacyBehavior>
+//                 <a className='block border border-gray-300/20 p-4 mb-4 z-50 rounded-lg'>
+//                   <span className='font-serif text-lg'>
+//                     {title}
+//                   </span>
+//                   <br/>
+//                   ({new Date(publishedAt).toDateString()})
+//                 </a>
+//               </Link>
+//             )
+//         )}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export async function getStaticProps() {
+//   const posts = await client.fetch(groq`
+//     *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+//   `)
+//   return {
+//     props: {
+//       posts
+//     }
+//   }
+// }
+
+// export default Index
+
+
+import React from 'react'
+
+const index = () => {
   return (
-    <div className='relative mx-4 md:mx-24 lg:mx-96 mt-16 md:mt-24 lg:mt-40'>
-      <h1 className='text-3xl mb-6'>Welcome to a blog!</h1>
-      <ul>
-        {posts.length > 0 && posts.map(
-          ({ _id, title = '', slug = '', publishedAt = '' }) =>
-            slug && (
-              <Link key={_id} href={`/blogs/stock_market/post/${encodeURIComponent(slug.current)}`} legacyBehavior>
-                <a className='block border border-gray-300/20 p-4 mb-4 z-50 rounded-lg'>
-                  <span className='font-serif text-lg'>
-                    {title}
-                  </span>
-                  <br/>
-                  ({new Date(publishedAt).toDateString()})
-                </a>
-              </Link>
-            )
-        )}
-      </ul>
-    </div>
+    <div>index</div>
   )
 }
 
-export async function getStaticProps() {
-  const posts = await client.fetch(groq`
-    *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
-  `)
-  return {
-    props: {
-      posts
-    }
-  }
-}
-
-export default Index
+export default index
