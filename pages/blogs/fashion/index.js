1751b4867b9f68c2427611b4259a09cc47ab5f5e
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-22 02:47:45 +0530

    Changed static to server side and added Scrollbar and fixed it

--- pages/blogs/fashion/index.js
+++ pages/blogs/fashion/index.js
@@ -8,27 +8,28 @@ import 'simplebar-react/dist/simplebar.min.css';
 const Index = ({ posts }) => {
   return (
     <div className="relative h-full">
-      <SimpleBar forceVisible="y" autoHide={true} className='overflow-visible overscroll-y-auto h-full'></SimpleBar>
-      <div className='relative mx-4 md:mx-24 lg:mx-96 mt-16 md:mt-24 lg:mt-40'>
-        <h1 className='text-3xl mb-6'>Welcome to a blog!</h1>
-        <ul>
-          {posts.length > 0 && posts.map(
-            ({ _id, title = '', slug = '', publishedAt = '' }) =>
-              slug && (
-                <Link key={_id} href={`/blogs/fashion/post/${encodeURIComponent(slug.current)}`} legacyBehavior>
-                  <a className='block border border-gray-300/20 p-4 mb-4 z-50 rounded-lg'>
-                    <span className='font-serif text-lg'>
-                      {title}
-                    </span>
-                    <br />
-                    ({new Date(publishedAt).toDateString()})
-                  </a>
-                </Link>
-              )
-          )}
-          {!posts.length > 0 && <p>No pots to show</p>}
-        </ul>
-      </div>
+      <SimpleBar forceVisible="y" autoHide={true} className='overflow-visible overscroll-y-auto h-full'>
+        <div className='relative mx-4 md:mx-24 lg:mx-96 mt-16 md:mt-24 lg:mt-40'>
+          <h1 className='text-3xl mb-6'>Welcome to a blog!</h1>
+          <ul>
+            {posts.length > 0 && posts.map(
+              ({ _id, title = '', slug = '', publishedAt = '' }) =>
+                slug && (
+                  <Link key={_id} href={`/blogs/fashion/post/${encodeURIComponent(slug.current)}`} legacyBehavior>
+                    <a className='block border border-gray-300/20 p-4 mb-4 z-50 rounded-lg'>
+                      <span className='font-serif text-lg'>
+                        {title}
+                      </span>
+                      <br />
+                      ({new Date(publishedAt).toDateString()})
+                    </a>
+                  </Link>
+                )
+            )}
+            {!posts.length > 0 && <p>No pots to show</p>}
+          </ul>
+        </div>
+      </SimpleBar>
     </div>
   )
 }
--- pages/blogs/fashion/post/[slug].js
+++ pages/blogs/fashion/post/[slug].js
@@ -1,11 +1,13 @@
 import groq from 'groq'
 import imageUrlBuilder from '@sanity/image-url'
-import {PortableText} from '@portabletext/react'
+import { PortableText } from '@portabletext/react'
 // import client from '../../../client'
 import { createClient } from "next-sanity";
 // import Image from 'next/image'
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
-function urlFor (source) {
+function urlFor(source) {
   return imageUrlBuilder(client).image(source)
 }
 
@@ -26,13 +28,13 @@ const ptComponents = {
   }
 }
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
 
   if (!post) {
     // Handle the case when post data is not available
     return <div>Loading...</div>; // You can render a loading state here
   }
-  
+
   const {
     title = 'Missing title',
     name = 'Missing name',
@@ -41,30 +43,34 @@ const Post = ({post}) => {
     body = []
   } = post
   return (
-    <article className="max-w-prose mx-auto p-4 mt-24 shadow-2xl">
-      <h1 className="text-3xl font-bold mb-2">{title}</h1>
-      <span className="text-gray-500">By {name}</span>
-      {categories && (
-        <ul className="mt-2 mb-4">
-          Posted in 
-          {categories.map((category) => (
-            <li key={category} className="inline-block mr-2 text-blue-500">
-              {category}
-            </li>
-          ))}
-        </ul>
-      )}
-      {authorImage && (
-        <div className="mb-4">
-          <img
-            className="w-12 h-12 rounded-full"
-            src={urlFor(authorImage).width(50).url()}
-            alt={`${name}'s picture`}
-          />
-        </div>
-      )}
-      <PortableText value={body} components={ptComponents} />
-    </article>
+    <div className="relative h-full">
+      <SimpleBar forceVisible="y" autoHide={true} className='overflow-visible overscroll-y-auto h-full'>
+        <article className="max-w-prose mx-auto p-4 mt-24 shadow-2xl pb-24 pt-4">
+          <h1 className="text-3xl font-bold mb-2">{title}</h1>
+          <span className="text-gray-500">By {name}</span>
+          {categories && (
+            <ul className="mt-2 mb-4">
+              Posted in
+              {categories.map((category) => (
+                <li key={category} className="inline-block mr-2 text-blue-500">
+                  {category}
+                </li>
+              ))}
+            </ul>
+          )}
+          {authorImage && (
+            <div className="mb-4">
+              <img
+                className="w-12 h-12 rounded-full"
+                src={urlFor(authorImage).width(50).url()}
+                alt={`${name}'s picture`}
+              />
+            </div>
+          )}
+          <PortableText value={body} components={ptComponents} />
+        </article>
+      </SimpleBar>
+    </div>
   );
 };
 
@@ -86,9 +92,9 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
 
 export async function getServerSideProps(context) {
   const { slug = "" } = context.query; // Note the change from `params` to `query`
-  
+
   const post = await client.fetch(query, { slug });
-  
+
   return {
     props: {
       post
