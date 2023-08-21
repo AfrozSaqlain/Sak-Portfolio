906f767ada4f41256274b7dc97bf29ca42bdd130
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-22 02:39:26 +0530

    Changed static to server side and added Scrollbar

--- pages/blogs/fashion/index.js
+++ pages/blogs/fashion/index.js
@@ -2,28 +2,33 @@ import Link from 'next/link'
 import groq from 'groq'
 // import client from '../../client'
 import { createClient } from "next-sanity";
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
 const Index = ({ posts }) => {
   return (
-    <div className='relative mx-4 md:mx-24 lg:mx-96 mt-16 md:mt-24 lg:mt-40'>
-      <h1 className='text-3xl mb-6'>Welcome to a blog!</h1>
-      <ul>
-        {posts.length > 0 && posts.map(
-          ({ _id, title = '', slug = '', publishedAt = '' }) =>
-            slug && (
-              <Link key={_id} href={`/blogs/fashion/post/${encodeURIComponent(slug.current)}`} legacyBehavior>
-                <a className='block border border-gray-300/20 p-4 mb-4 z-50 rounded-lg'>
-                  <span className='font-serif text-lg'>
-                    {title}
-                  </span>
-                  <br/>
-                  ({new Date(publishedAt).toDateString()})
-                </a>
-              </Link>
-            )
-        )}
-        {!posts.length > 0 && <p>No pots to show</p>}
-      </ul>
+    <div className="relative h-full">
+      <SimpleBar forceVisible="y" autoHide={true} className='overflow-visible overscroll-y-auto h-full'></SimpleBar>
+      <div className='relative mx-4 md:mx-24 lg:mx-96 mt-16 md:mt-24 lg:mt-40'>
+        <h1 className='text-3xl mb-6'>Welcome to a blog!</h1>
+        <ul>
+          {posts.length > 0 && posts.map(
+            ({ _id, title = '', slug = '', publishedAt = '' }) =>
+              slug && (
+                <Link key={_id} href={`/blogs/fashion/post/${encodeURIComponent(slug.current)}`} legacyBehavior>
+                  <a className='block border border-gray-300/20 p-4 mb-4 z-50 rounded-lg'>
+                    <span className='font-serif text-lg'>
+                      {title}
+                    </span>
+                    <br />
+                    ({new Date(publishedAt).toDateString()})
+                  </a>
+                </Link>
+              )
+          )}
+          {!posts.length > 0 && <p>No pots to show</p>}
+        </ul>
+      </div>
     </div>
   )
 }
@@ -36,13 +41,14 @@ const client = createClient({
 })
 
 
-export async function getStaticProps() {
-  const posts = await client.fetch(groq`*[_type == "post" && publishedAt < now()] | order(publishedAt desc)`)
+export async function getServerSideProps() {
+  const posts = await client.fetch(groq`*[_type == "post" && publishedAt < now()] | order(publishedAt desc)`);
+
   return {
     props: {
       posts
     }
-  }
+  };
 }
 
 export default Index
