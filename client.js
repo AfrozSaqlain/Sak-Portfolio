28bebbb6fd477a5ac77523b59dd2a05d9702a830
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-21 12:38:29 +0530

    Tried setting published at date to before

--- client.js
+++ client.js
@@ -3,7 +3,8 @@ import { createClient } from '@sanity/client'
 const client = createClient({
     projectId: 'n2x4qif3',
     dataset: 'production',
-    useCdn: true
+    useCdn: true,
+    apiVersion: '2021-03-25', 
 })
 
 export default client
