7bae316598fe7de16b36503cf4eb967d11e2bcf2
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-23 02:08:42 +0530

    Some tweaks in notes again

--- pages/academics/electromag/index.js
+++ pages/academics/electromag/index.js
@@ -40,7 +40,7 @@ const electromag = () => {
         <div className="relative h-full">
             <div className="h-full translate-y-10 overflow-y-auto overflow-visible overscroll-y-auto pb-24 pt-4">
                 <SimpleBar forceVisible="y" autoHide={true} className='overflow-visible overscroll-y-auto h-full'>
-                    <div className="max-w-4xl mx-auto p-4 shadow-2xl rounded-lg">
+                    <div className="max-w-4xl mx-auto mb-12 p-4 shadow-2xl rounded-lg">
                         <h1 className="text-2xl font-semibold text-center font-mono justify-center text-accent mb-4">Electricity and Magnetism</h1>
                         {sections.map((section, index) => (
                             <ExpandableSection key={index} title={`${index + 1}. ${section.title}`}>
