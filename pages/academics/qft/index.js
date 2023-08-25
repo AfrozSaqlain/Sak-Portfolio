5ca58d42ad916ae1eb532d4f3919749dde618848
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-08-25 13:51:17 +0530

    Added anmation in sections

--- pages/academics/qft/index.js
+++ pages/academics/qft/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import MathJaxWrapper from '../../../components/MathJaxWrapper';
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
+import { ExpandableSection, Subsection } from '../../../components/ExpandableSection';
 // import ScrollToTopArrow from '../../../components/ScrollToTopArrow';
 
 const qft = () => {
@@ -338,72 +339,6 @@ const qft = () => {
                 </SimpleBar>
                 {/* <ScrollToTopArrow /> */}
             </div>
-        </div>
-    );
-};
-
-const ExpandableSection = ({ title, children }) => {
-    const [expanded, setExpanded] = useState(false);
-
-    return (
-        <div className="mb-4">
-            <div
-                className="flex justify-between items-center cursor-pointer"
-                onClick={() => setExpanded(!expanded)}
-            >
-                <h2 className="text-lg font-semibold">{title}</h2>
-                <div
-                    className={`ml-2 transition-transform transform ${expanded ? 'rotate-[-90deg]' : 'rotate-0'
-                        }`}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-4 h-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M4.293 5.293a1 1 0 011.414 0L10 9.586l4.293-4.293a1 1 0 111.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
-                </div>
-            </div>
-            {expanded && <div className="mt-2">{children}</div>}
-        </div>
-    );
-};
-
-const Subsection = ({ title, children }) => {
-    const [expanded, setExpanded] = useState(false);
-
-    return (
-        <div className={`mb-2 pl-3`}>
-            <div
-                className="flex justify-between items-center cursor-pointer"
-                onClick={() => setExpanded(!expanded)}
-            >
-                <h3 className="text-md font-semibold">{title}</h3>
-                <div
-                    className={`ml-6 transition-transform transform ${expanded ? 'rotate-[-90deg]' : 'rotate-0'
-                        }`}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-4 h-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M4.293 5.293a1 1 0 011.414 0L10 9.586l4.293-4.293a1 1 0 111.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
-                </div>
-            </div>
-            {expanded && <div className="mt-1">{children}</div>}
         </div>
     );
 };
