d7bfadb1f49e7d8121bc7484c104e3548f6e8f7a
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-09-01 23:52:36 +0530

    z-20 for Projects-Btn

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -38,7 +38,7 @@ import { HiArrowRight } from 'react-icons/hi2';
 
 const ProjectsBtn = () => {
   return (
-    <div className='w-[135px] h-[135px] z-50 mx-auto xl:mx-0 -translate-y-11'>
+    <div className='w-[135px] h-[135px] z-20 mx-auto xl:mx-0 -translate-y-11'>
       <Link href='/projects'>
         <div className='relative w-full h-full flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group'>
           <Image
