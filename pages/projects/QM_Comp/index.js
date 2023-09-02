2cb23fcf7781760892034c368d2481bf8319947b
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-09-02 22:37:17 +0530

    Added codesandbox with some styling

--- pages/projects/QM_Comp/index.js
+++ pages/projects/QM_Comp/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
 const CodeSandboxEmbed = ({ src }) => {
   return (
@@ -8,7 +10,7 @@ const CodeSandboxEmbed = ({ src }) => {
       width="80%"
       height="600px" // You can adjust the height as needed
       style={{ border: '0', borderRadius: '4px', overflow: 'hidden' }}
-      className='translate-x-24 translate-y-20'
+      className='translate-x-28 translate-y-20'
       sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
     ></iframe>
   );
@@ -19,13 +21,15 @@ const App = () => {
   const codeSandboxSrc = 'https://codesandbox.io/s/fancy-https-nty67m';
 
   return (
-    <div>
-      <h1 className='translate-y-16 translate-x-24 font-semibold'>CodeSandbox Embed Example</h1>
-      <CodeSandboxEmbed src={codeSandboxSrc} />
+    <div className="relative h-full">
+      <div className="h-full translate-y-10 overflow-y-auto overflow-visible overscroll-y-auto pb-20 pt-4">
+        <SimpleBar forceVisible="y" autoHide={true} className='overflow-visible overscroll-y-auto h-full'>
+          <h1 className='translate-y-16 text-center items-center sm:translate-x-7 font-semibold'>CodeSandbox Embed Example</h1>
+          <CodeSandboxEmbed src={codeSandboxSrc} />
+        </SimpleBar>
+      </div>
     </div>
   );
 };
 
 export default App;
-
-
