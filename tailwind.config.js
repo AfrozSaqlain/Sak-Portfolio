b2ed85607b3a48a06c4a1c9844c7fec1529ac46b
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-09-03 01:38:37 +0530

    Reverted back to main image of bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,6 @@ module.exports = {
         circles: 'url("/bg-circles.png")',
         circleStar: 'url("/circle-star.svg")',
         site: 'url("/site-bg.svg")',
-        space: 'url("/nebula.jpg")',
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
