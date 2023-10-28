25b8b1bc725728771cc4e1325e96361a4c87f7c2
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-10-28 14:06:23 +0530

    Some tweaks in lang again 5

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
             width={220}
             height={48}
             alt='logo'
-            priority={true}
+            // priority={true}
             className="hidden lg:block"
           />
         </Link>
