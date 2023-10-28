36b28d4bfcb63928743d1b74c9cc4226839bc5a9
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-10-28 22:27:52 +0530

    Added discernible name to Logo Links

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
     <div className="container mx-auto">
       <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
         {/* logo */}
-        <Link href={'/'}>
+        <Link href={'/'} aria-label="Home">
           <Image
             src={'/logo.svg'}
             width={220}
