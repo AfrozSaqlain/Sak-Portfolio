99d3274cd764cf65ae9c1c0647d2f7e7786bdc96
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-12-21 16:59:21 +0530

    Updated few things 2.0

--- pages/academics/index.js
+++ pages/academics/index.js
@@ -15,11 +15,11 @@ const Academics = () => {
     <div className='h-full bg-primary/30 py-36 flex items-center'>
       <Circles />
       <div className="container mx-auto">
-        <div className='flex flex-col xl:flex-row gap-x-8'>
+        <div className='flex flex-col xl:flex-row gap-x-8 -translate-y-5'>
           {/* text */}
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
             <motion.h2 variants={fadeIn('up', 0.3)} initial='hidden' animate='show' exit='hidden' className='h2 xl:mt-12'>My Lectures <span className='text-accent'>.</span></motion.h2>
-            <motion.p variants={fadeIn('up', 0.4)} initial='hidden' animate='show' exit='hidden' className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Welcome to my projects page! Here you&rsquo;ll find all my projects, which include notes from my college courses and independent works. I hope that my projects can inspire others and help them learn something new. If you have any questions or need any help with your own projects, feel free to ask me! </motion.p>
+            <motion.p variants={fadeIn('up', 0.4)} initial='hidden' animate='show' exit='hidden' className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Welcome to my projects page! Here you&rsquo;ll find all my notes from my college courses and independent study. I hope that this helps other learn something new. If you have any questions or need any help with your own projects, feel free to ask me! </motion.p>
           </div>
 
           {/* slider */}
