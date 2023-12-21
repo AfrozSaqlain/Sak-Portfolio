c4fc483afeeb67111a262c97e7e6164f5ea953d1
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2023-12-21 18:25:25 +0530

    Updated dg notes

--- pages/academics/Differential_Geometry/index.js
+++ pages/academics/Differential_Geometry/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import MathJaxWrapper from '../../../components/MathJaxWrapper';
 import { ExpandableSection, Subsection } from '../../../components/ExpandableSection';
+import Image from 'next/image';
 
 const Electromag = () => {
     const sections = [
@@ -9,7 +10,63 @@ const Electromag = () => {
             subsections: [
                 {
                     title: 'Introduction',
-                    content: `When \\(a \\ne 0\\), there are two solutions to \\(ax^2 + bx + c = 0\\) and they are $$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$`
+                    content: `
+                    Elementary calculus works best for functions like \\( f : \\mathbb{R} \\to \\mathbb{R} \\), and with some tweaks we can make clculus work for functions like \\( g : \\mathbb{R}^m \\to \\mathbb{R}^n \\). Howver, in physics we need much more general functions like \\( h : U \\to V \\). Elementary calculus cannot be used in this sense, unless we have some help and that help is provided by Differentiable Manifolds.
+                    <br/>
+                    <br/>
+                    Lets' recallthe definition of derivative from our elementary calculus 
+                    \\[
+                        f'(x) = \\lim_{h \\to 0} \\frac{f(x_0 + h) - f(x_0)}{h}    
+                    \\]
+
+                    Some properties of real numbers that have been used in this definition are:
+                    <ul class='list-disc ml-8 my-4'>
+                        <li class="mb-2"> Cauchy Completeness </li>
+                        <li class="mb-2"> \\( x_0 + h \\implies \\) addition must make sense (on domain) </li>
+                        <li class="mb-2"> \\( f(x_0 + h) - f(x_0) \\implies \\) subtraction must work (on the range) </li>
+                        <li class="mb-2"> \\( \\frac{f(x_0 + h) - f(x_0)}{h} \\implies \\) division must work </li>
+                    </ul>
+
+                    Now let's see what's the problem in doing calculus on \\( f : \\mathbb{R}^m \\to \\mathbb{R}^n \\)
+                    \\[
+                        \\lim_{\\vec{h} \\to 0} \\frac{\\vec{f}(\\vec{x_0} + \\vec{h}) - \\vec{f}(\\vec{x_0})}{\\vec{h}}    
+                    \\]
+
+                    The problem is that we cannot devide one vector with another of arbitrary dimensions.
+
+                    <p class='text-center text-lg text underline underline-offset-8 mt-5 mb-2 font-semibold text-blue-500'>The Differential</p>
+
+                    Key Idea: A small enough piece of any smooth curve is like a straight line.
+                    <Image
+                        src="/differential_geometry/differential.png"
+                        alt="differential"
+                        width={10}
+                        height={50}
+                        class="mx-auto scale-75 rounded-lg"
+                    />
+                    <ul class='list-disc ml-8 my-4'>
+                        <li class="mb-2"> \\( f(x+h) = f(x) + f'(x)h + \\Delta \\) where \\( \\Delta \\) vanishes very fast as \\( h \\to 0 \\) for a smooth curve, <br/> 
+                        \\[
+                            \\lim_{|h| \\to 0} \\frac{|\\Delta|}{|h|} = 0
+                        \\]
+                        </li>
+                        <li class="mb-2"> \\( f'(x)h \\) is the value of a map that takes h to \\( f'(x)h \\): a linear map ( let's call it \\( Df|_x \\) ) </li>
+                    </ul>
+
+                    <p class='text-lg text underline underline-offset-8 mt-5 mb-2 font-semibold text-blue-500'>Equivalent definition of differentiability</p>
+                    A function \\( f : \\mathbb{R} \\to \\mathbb{R} \\) is differentiable at \\( x \\in \\mathbb{R} \\), if \\( \\exists \\) a linear map \\( Df|_x : \\mathbb{R} \\to \\mathbb{R} \\) such that 
+                    \\[
+                        \\lim_{|h| \\to 0} \\frac{| f(x+h) -f(x) - Df|_x (h) |}{|h|} = 0
+                    \\] 
+                    where \\( Df|_x (h) = f'(x)h \\). And we can easilty prmote this definition for a function of form \\( f: \\mathbb{R}^m \\to \\mathbb{R}^n \\).
+                    <br/>
+                    <br/>
+                    A function \\( f: \\mathbb{R}^m \\to \\mathbb{R}^n \\) is differentiable at \\( x \\in \\mathbb{R}^m \\), if \\( \\exists \\) a linear map \\( Df|_x : \\mathbb{R}^m \\to \\mathbb{R}^n \\) such that 
+                    \\[
+                        \\lim_{||h|| \\to 0} \\frac{|| f(x+h) -f(x) - Df|_x (h) ||}{||h||} = 0
+                    \\] 
+
+                    `
                 },
                 {
                     title: 'Equations',
